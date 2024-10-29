e0e7e7ca15d5661083623ea6e7c97262b4198a39
Deepesh-Pundir <deepeshpundir@gmail.com>
2024-10-29 20:41:27 +0530

    create env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,12 +7,20 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBoOAh4c2r69txEd92w4uQq8vkmeJJBw1g",
-  authDomain: "react-authentication-app-4de23.firebaseapp.com",
-  projectId: "react-authentication-app-4de23",
-  storageBucket: "react-authentication-app-4de23.appspot.com",
-  messagingSenderId: "332295975268",
-  appId: "1:332295975268:web:c169444a4b46436826ca71"
+  // apiKey: "AIzaSyBoOAh4c2r69txEd92w4uQq8vkmeJJBw1g",
+  // authDomain: "react-authentication-app-4de23.firebaseapp.com",
+  // projectId: "react-authentication-app-4de23",
+  // storageBucket: "react-authentication-app-4de23.appspot.com",
+  // messagingSenderId: "332295975268",
+  // appId: "1:332295975268:web:c169444a4b46436826ca71"
+
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  // databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
